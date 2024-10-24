4d465cbf483503b17ecf58941e5eaffa1ce22e2a
Sathvika-27 <sathvika.murikipudi27@gmail.com>
2024-10-24 18:28:38 +0530

    added

---
+++ sathvika-arthematicop.js
@@ -0,0 +1,12 @@
+function arthematicop(x,y){
+    
+    console.log("addition of 2 nos="+(x+y));
+    console.log("subtraction of 2 nos="+(x-y));
+    console.log("multiplication of 2 nos="+(x*y));
+    console.log("division of 2 nos="+(x/y));
+    console.log("modulusdivision of 2 nos="+(x%y));
+}
+
+arthematicop(3,4);
+
+arthematicop(6,9);
