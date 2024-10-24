b1791a546c820d947b15ee2f7b85b843857ad089
chandra-006 <craju852292@gmail.com>
2024-10-24 18:26:50 +0530

    operators

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ console.log("hello");
 function add(a ,b){
     var a;
     var b;
-    let z = a + b * a ;
-    console.log("the value of a and b:" +z)
+    let z = a + b ;
+    let n = a * b ;
+    let m = a - b ;
+    let q = a / b ;
+    let p = a % b ;
+    console.log("the value of a and b:" +z ,+n ,+m ,+q, +p);
 }
 add(2, 3);
