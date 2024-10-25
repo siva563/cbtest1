fc8c938fcc4be24b33695ae38ba0595a3dca3d2e
chandra-006 <craju852292@gmail.com>
2024-10-25 12:18:59 +0530

    operators

--- script.js
+++ script.js
@@ -1,13 +1,75 @@
-console.log("hello");
+//console.log("hello");
 
-function add(a ,b){
+/*function add(a ,b){
     var a;
     var b;
     let z = a + b ;
-    let n = a * b ;
-    let m = a - b ;
-    let q = a / b ;
-    let p = a % b ;
-    console.log("the value of a and b:" +z ,+n ,+m ,+q, +p);
+  
+    console.log("the value of a and b:" +z);
 }
 add(2, 3);
+
+var x = 10 ;
+var y = 20;
+ function cool( a,b){
+    var c=x+y;
+    console.log(c);
+ }
+ cool(x,y);*/
+
+/*var x = 10 ;
+var y = 20;
+ function cool( a,b){
+    x =a;
+    y=b;
+    var z=x+y;
+    console.log(z);
+ }
+ cool(2,5);*/
+   var x=5;
+   var y=2;
+ function add(a,b){
+   console.log("the addition of a and b="+(a+b));
+   }
+ add(2,3);
+
+ function sub(a,b){
+   console.log("the subtraction of a and b="+(a-b));
+ }
+ sub(10,5);
+
+ function mul(a,b){
+   console.log("the multiplication of a and b="+(a*b));
+ }
+ mul(10,5);
+
+ function div(a,b){
+   console.log("the division of a and b="+(a/b));
+ }
+ div(10,5);
+
+ function modulus(a,b){
+   console.log("the modulus of a and b="+(a%b));
+ }
+ modulus(3,1);
+
+ console.log("hello ->")
+
+var a=4;
+a++;
+console.log(a);
+
+var b=5;
+b--;
+console.log(b);
+
+var c=3;
+++c;
+console.log(c);
+
+var d=10;
+--d;
+console.log(d);
+
+
+
