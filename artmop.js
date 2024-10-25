0e06a0e4771974a9434fd0a94865a67a80e667c0
Shamitha <darlingshamitha02@gmail.com>
2024-10-25 18:17:24 +0530

    added

--- artmop.js
+++ artmop.js
@@ -1,13 +1,23 @@
-function arithematicoperations(a,b){
+function add(a,b){
     console.log("addition is : "+(a+b));
+}
+function sub(a,b){
     console.log("subtraction is : "+(a-b));
+}
+function mul(a,b){
     console.log("multiplication is : "+(a*b));
+}
+function div(a,b){
     console.log("division is : "+(a/b));
+}
+function mod(a,b){
     console.log("modulus is : "+(a%b));
 }
-
-arithematicoperations(10,5);
-
-arithematicoperations(15,5);
-
-arithematicoperations(30,5);
+add(100,50);
+sub(5,3);
+div(10,2);
+mul(4,8);
+mod(30,7);
+add(20,7);
+mul(8,6);
+mod(82,4);
